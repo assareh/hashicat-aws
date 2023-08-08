009a0c21ade8d4fccb25fe19ffc226935b0ee198
Andy Assareh <assareh@hashicorp.com>
2023-08-07 22:22:48 -0700

    Revert "adding doormat provider"
    This reverts commit ed20a8f848515522530e5f5ba7df750dc692d8c6.

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
-provider "aws" {
-  access_key = data.doormat_aws_credentials.creds.access_key
-  secret_key = data.doormat_aws_credentials.creds.secret_key
-  token      = data.doormat_aws_credentials.creds.token
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "=3.42.0"
+    }
+  }
+}
 
-  region = var.region
+provider "aws" {
+  region  = var.region
 }
 
 resource "aws_vpc" "hashicat" {
